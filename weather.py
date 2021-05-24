79e79df1debab4b2d3827a9a3df44c30759205e1
Reekraj Roy <55712612+REEKRAJROY@users.noreply.github.com>
2021-05-24 20:40:59 +0530

    Delete weather.py

--- weather.py
+++
@@ -1,8 +0,0 @@
-#using open weather api and requests library
-import requests
-from pprint import pprint
-API_Key = 'key'
-city = input('Enter a city: ')
-base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city
-weather_data = requests.get(base_url).json()
-pprint(weather_data)
